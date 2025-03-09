615c23f9a84a36c36e93dca7fb932cfdf6f58028
AlexTimofeev <lesha.timofeev2017@yandex.ru>
2025-03-09 22:29:57 +0300

    init commit

---
+++ src/main/java/ru/timofeev/project_developing/ProjectDevelopingApplication.java
@@ -0,0 +1,13 @@
+package ru.timofeev.project_developing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDevelopingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDevelopingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/timofeev/project_developing/ProjectDevelopingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.timofeev.project_developing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDevelopingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
